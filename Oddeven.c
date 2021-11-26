4ac99fd7820ef781e0a024261fc8af1b0aaefa99
Monu Chauhan <68127777+p4programer@users.noreply.github.com>
2021-11-26 18:08:48 +0530

    Add files via upload

---
+++ Oddeven.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n;
+    printf("Enter number A = ");
+    scanf("%d",&n);
+
+    if(n%2 == 0)
+     printf("Even\n");
+     else
+     printf("Odd\n");
+
+     return 0;
+
+}
